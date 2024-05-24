161d99a134c164e0df52ec7d1631f53f3f47ba09
Sup30java <priyasupriya638@gmail.com>
2024-05-24 17:11:43 +0530

    Update UserInputProgram.java

--- Task-1/UserInputProgram.java
+++ Task-1/UserInputProgram.java
@@ -7,13 +7,29 @@ public static void main(String[] args) {
 // Creating a scanner object to read the input
 Scanner sc = new Scanner(System.in);
 
-// Prompts the user to enter a boolean value
-System.out.println(" Enter a boolean value (true/false): ");
-boolean userInput = sc.nextBoolean();
+// Prompts the user to enter first integer
+System.out.println(" Enter the first integer is: ");
+int num1 = sc.nextInt();
 
-// Display the opposite of the entered boolean value
-boolean Oppositevalue = !userInput;
-System.out.println(" The opposite of true is:" + Oppositevalue);
+// Prompts the user to enter second integer
+System.out.println(" Enter the second integer is: ");
+int num2 = sc.nextInt();
+
+// Calculate the sum, difference, and product
+
+int sum = num1 + num2;
+int difference = num1 - num2;
+int product = num1 * num2;
+       
+// Prompts the user to enter floating integer
+System.out.println("Enter a floating-point number:");
+float number = sc.nextFloat();
+
+// Multiply the floating-point number by 2
+float result = number * 2;
+
+// display the result
+System.out.println(number + " multiplied by 2 is:" + result);       
 
 // Prompts the user to enter a single character
 
@@ -25,32 +41,20 @@ char inputchar = sc.next().charAt(0);
 char nextchar = (char) (inputchar + 1);
 System.out.println("The next character after '" + inputchar + "' is: " + nextchar);
 
+// Prompts the user to enter a boolean value
+System.out.println(" Enter a boolean value (true/false): ");
+boolean userInput = sc.nextBoolean();
 
+// Display the opposite of the entered boolean value
+boolean Oppositevalue = !userInput;
+System.out.println(" The opposite of true is:" + Oppositevalue);
 
-// Prompts the user to enter floating integer
-System.out.println("Enter a floating-point number:");
-float number = sc.nextFloat();
+// Prompts the user to enter their name
+System.out.println("Enter your name: ");
+String name = sc.nextLine();
 
-// Multiply the floating-point number by 2
-float result = number * 2;
-
-// display the result
-System.out.println(number + " multiplied by 2 is:" + result);
-
-
-// Prompts the user to enter first integer
-System.out.println(" Enter the first integer is: ");
-int num1 = sc.nextInt();
-
-// Prompts the user to enter second integer
-System.out.println(" Enter the second integer is: ");
-int num2 = sc.nextInt();
-
-       // Calculate the sum, difference, and product
-
-int sum = num1 + num2;
-int difference = num1 - num2;
-int product = num1 * num2;
+// Display a greeting message that includes the user's name
+System.out.println(" hello," + name + "! ");
 
 // display the sum, difference, and product
 System.out.println("Sum of " + num1 + " and " + num2 + " is :" + sum);
@@ -60,12 +64,7 @@ System.out.println("Product of " + num1 + " and " + num2 + " is : " +product);
 // Consume the newline left-over
         sc.nextLine();
 
-// Prompts the user to enter their name
-System.out.println("Enter your name: ");
-String name = sc.nextLine();
 
-// Display a greeting message that includes the user's name
-System.out.println(" hello," + name + "! ");
 
 
 
